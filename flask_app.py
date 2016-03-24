663d9fdae31831bbec9a4fdd5e6c5947f710bfd7
caveligh <cesavegl@gmail.com>
2016-03-24 00:12:08 +0000

    Quinto commit. Config de la BD

--- flask_app.py
+++ flask_app.py
@@ -2,9 +2,22 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+# Conecciona la BD
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="inticayapy",
+    password="MSql2016",
+    hostname="inticayapy.mysql.pythonanywhere-services.com",
+    databasename="inticayapy$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+db = SQLAlchemy(app)
+
+# Fin DB Conecction
 comments = []
 @app.route("/", methods=["GET", "POST"])
 def index():
