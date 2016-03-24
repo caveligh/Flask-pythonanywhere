74303de05b6470f64c5d3f35bc4b3d1fcef0d778
caveligh <cesavegl@gmail.com>
2016-03-24 00:44:57 +0000

    Septimo commit. Incorporar R/W en  tabla comments

--- flask_app.py
+++ flask_app.py
@@ -30,7 +30,11 @@ class Comment(db.Model):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+#        return render_template("main_page.html", comments=comments)
+        return render_template("main_page.html", comments=Comment.query.all())
 
-    comments.append(request.form["contents"])
+#    comments.append(request.form["contents"])
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
     return redirect(url_for('index'))
